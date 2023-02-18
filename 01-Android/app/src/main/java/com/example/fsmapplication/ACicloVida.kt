6c1f26b328333bdf0c465501300a061141970fbd
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-02-17 23:12:34 -0500

    Ciclo de Vida (On Restart On Resume)

--- 01-Android/app/src/main/java/com/example/fsmapplication/ACicloVida.kt
+++ 01-Android/app/src/main/java/com/example/fsmapplication/ACicloVida.kt
@@ -13,6 +13,20 @@ class ACicloVida : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityAcicloVidaBinding
+    var textoGlobal=""
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun mostrarSnackbar(texto:String){
+        textoGlobal+=texto
+        Snackbar.make(findViewById(R.id.cl_ciclo_vida), //localizamos el coordinator layaut de la actividad destino
+            textoGlobal,Snackbar.LENGTH_LONG)
+            .setAction("Action",null).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +44,55 @@ class ACicloVida : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        mostrarSnackbar("onCreate")
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("onStart")
     }
-}
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+        mostrarSnackbar("")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run{
+            //guardar las variables
+            //primitivas
+            putString("textoGuardado", textoGlobal)
+            //putInt("numeroGuardado",numero)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val textoRecuperado:String? = savedInstanceState.getString("textoGuardado")
+        //val textoREcuperado: Int? = savedInstanceState.getString("")
+        if(textoRecuperado != null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
+    }
+
+
+} //onCreate fin de bloque de codigo
--- 01-Android/app/src/main/java/com/example/fsmapplication/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/fsmapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.fsmapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCicloVida= findViewById<Button>(R.id.btn_ciclo_vida) //Reconozco el Id del componente
+        botonCicloVida
+            .setOnClickListener{        //Alhacer click ir hacia la actividad ACicloVida
+                irActividad(ACicloVida::class.java)
+            }
+    }
+    fun irActividad(            //Funcion ir Actividad //Intends
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
