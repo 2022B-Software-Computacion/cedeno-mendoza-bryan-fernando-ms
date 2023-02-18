43ccc1f9ccda0d8c075428a574811050094867df
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-02-18 14:39:06 -0500

    Crud de Entrenadores

--- 01-Android/app/src/main/java/com/example/fsmapplication/ESqliteHelperEntrenador.kt
+++ 01-Android/app/src/main/java/com/example/fsmapplication/ESqliteHelperEntrenador.kt
@@ -48,7 +48,83 @@ class ESqliteHelperEntrenador(
         return if(resultadoGuardar.toInt() == -1) false else true // retornamos el estado de la accion
     }
 
-    
+    fun eliminarEntrenadorFormulario(id:Int):Boolean{
+        val conexionEscritura=writableDatabase
+        val resultadoEliminacion = conexionEscritura
+            .delete(        //eliminar en la tabla entrenador
+                "ENTRENADOR", //NOMBRE DE TABLA
+                "id=?",        //Consulta where
+                arrayOf(
+                    id.toString()
+                )//Parametros
+            )
+        conexionEscritura.close() //Cerramos la coneccion de la base de datos
+        return if(resultadoEliminacion.toInt() == -1) false else true // retornamos el estado de la accion
+    }
+
+    fun actualizarEntrenadorFormulario(
+        nombre:String,
+        descripcion: String,
+        idActualizar:Int
+    ):Boolean{
+        val conexionEscritura = writableDatabase
+        val valoresActualizar = ContentValues()
+        valoresActualizar.put("nombre", nombre)
+        valoresActualizar.put("descripcion", descripcion)
+        val resultadoActualizacion = conexionEscritura
+            .update(  // Accion a realizar
+                "ENTRENADOR",
+                valoresActualizar,
+                "id=?", //Clausula Where
+                arrayOf(
+                    idActualizar.toString() //parametro a comparar
+                )
+            )
+        conexionEscritura.close()
+        return if (resultadoActualizacion == -1)false else true
+    }
+
+    fun consultarEntrenadorPorId(id: Int):BEntrenador{
+        //val baseDatosLecutra = this.readableDatabase
+        val baseDatosLectura = readableDatabase
+        val scriptConsultarUsuario = "SELECT * FROM ENTRENADOR WHERE ID =?" //ponemos la consulta con los parametros que necesitemos
+        val resultadoConsultaLectura = baseDatosLectura.rawQuery(
+            scriptConsultarUsuario, //script de Consulta
+            arrayOf(
+                id.toString()
+            )//Parametros de la consulta
+        )
+        //Logica busqueda
+        val existeUsuario = resultadoConsultaLectura.moveToFirst() //Nos movemos al primero dato de la consulta
+        var usuarioEncontrado = BEntrenador(0,"","") //Crearemos un entrenador
+
+        val arreglo = arrayListOf<BEntrenador>()//En el caso de que lo que querramos es un arerglo de entrenadores
+                                                //y luego podemos agregar cada entrenador  al array
+        if(existeUsuario){
+            do{
+                val id = resultadoConsultaLectura.getInt(0)//columna indice 0->id
+                val nombre = resultadoConsultaLectura.getString(1) //Columna indice 1->Nombre
+                val descripcion  = resultadoConsultaLectura.getString(2) //columna indice 2->descripción
+                if(id != null){
+                   // usuarioEncontrado = BEntrenador(0,"","")//si lo que se instancia es el arreglo
+                    usuarioEncontrado.id = id
+                    usuarioEncontrado.nombre = nombre
+                    usuarioEncontrado.descripcion = descripcion
+                    arreglo.add(usuarioEncontrado)
+                }
+            }while (resultadoConsultaLectura.moveToNext()) //mientras nos podemos mover al sig instancia
+        }
+        resultadoConsultaLectura.close()
+        baseDatosLectura.close()
+        return usuarioEncontrado
+    }
+
+
+
+
+
+
+
 
 
 }
