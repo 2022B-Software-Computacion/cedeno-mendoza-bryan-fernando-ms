840a2a61104d3d55acceac8394a74b53024fe38b
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-02-18 10:01:28 -0500

    Menu Contextuales

--- 01-Android/app/src/main/java/com/example/fsmapplication/BListView.kt
+++ 01-Android/app/src/main/java/com/example/fsmapplication/BListView.kt
@@ -1,13 +1,21 @@
 package com.example.fsmapplication
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 
 class BListView : AppCompatActivity() {
     val arreglo = BBaseDatosMemoria.arregloBEntrenador
+    var idItemSeleccionado = 0  // variable que me permite identificar que item del viewlist seleccione
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +35,79 @@ class BListView : AppCompatActivity() {
             .setOnClickListener {
                 anadirEntrenador(adaptador)         //seteamos los datos
             }
+        registerForContextMenu(listView) // Cuando den clic sobre el list view se mostrara algo.
+
+
     }
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        val inflater = menuInflater     //muestra el xml que tenemos con el menu
+        inflater.inflate(R.menu.menu, menu) // trabaja con el menu llamado menu
+        //Obtener el id de ArrayListSeleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position  // guardamos ese id como el item seleccionado
+        idItemSeleccionado = id
+    }
+
+    //Acciones cuando se selecciona un elemento del menu contextual.
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.mi_editar  ->{
+                "${idItemSeleccionado}"
+                return true
+            }
+            R.id.mi_eliminar ->{
+                abrirDialogo() //Abrimos dialogo
+                "${idItemSeleccionado}"
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
+
+    fun abrirDialogo(){         //Existen varios tipos de dialogos como opcion multiple o solo una opcion, aceptar denegar,etc
+        val builder = AlertDialog.Builder(this)  //la clase builder me permite crear los dialogos
+        builder.setTitle("Desea eliminar")          //creamos titulo
+        builder.setPositiveButton(                  //seteamos boton
+            "Aceptar",                              //funcionalidad positiva
+            DialogInterface.OnClickListener{ dialog, which ->
+                //al aceptar eliminar el registro
+            }
+        )
+        builder.setNegativeButton(
+            "Cancelar",
+            null
+        )
+        val opciones = resources.getStringArray(        //realizo opciones
+            R.array.string_array_opciones_dialogo
+        )
+        val seleccionPrevia = booleanArrayOf(           //realizamos una opcion previa o por defecto
+            true, //lunes seleccionado
+            false, //martes no seleccionado
+            false //miercoles no seleccionado
+        )
+        builder.setMultiChoiceItems(        //ponemos las opciones y la seleccion previa en el dialogo
+            opciones,
+            seleccionPrevia,
+            {
+                    dialog,
+                    which,
+                    isChecked ->
+                "Dio click en el item ${which}"
+            }
+        )
+        val dialogo = builder.create()        ///creamos y mostramos el dialogo
+        dialogo.show()
+    }
+
+
     fun anadirEntrenador(
         adaptador: ArrayAdapter<BEntrenador>  //Llega un adaptador que es un array de entredadores
     ){
@@ -39,4 +119,6 @@ class BListView : AppCompatActivity() {
         )
         adaptador.notifyDataSetChanged()   //
     }
+
+
 }
