430fbbdd44bb01fa651ede02c30c63d5d7a3f220
Fernando Cedeño <bryan.cedeno01@epn.edu.ec>
2023-02-22 00:43:48 -0500

    Charts terminado

--- DeberShazam/app/src/main/java/com/example/cloneshazam/ParentAdapter.kt
+++ DeberShazam/app/src/main/java/com/example/cloneshazam/ParentAdapter.kt
@@ -1,10 +1,15 @@
 package com.example.cloneshazam
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,6 +22,8 @@ class ParentAdapter(private val parentList: List<ParentItemD>) :
     inner class ParentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTv: TextView = itemView.findViewById(R.id.tv_parentTitle)
         val childRecyclerView: RecyclerView = itemView.findViewById(R.id.langRecyclerView)
+        val seeallButton = itemView.findViewById<Button>(R.id.btn_seeAll)
+        val constLayout = itemView.findViewById<ConstraintLayout>(R.id.ct_layaut)
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -35,13 +42,28 @@ class ParentAdapter(private val parentList: List<ParentItemD>) :
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: ParentViewHolder, position: Int) {
-        val parentItem = parentList[position]
-        holder.titleTv.text = parentItem.title
-        holder.childRecyclerView.setHasFixedSize(true)
-        holder.childRecyclerView.layoutManager = GridLayoutManager(holder.itemView.context, 3)
-        val adapter = ChildAdapter(parentItem.mList)
-        holder.childRecyclerView.adapter = adapter
+        if (position==0){
+            holder.titleTv.visibility=View.GONE
+            holder.seeallButton.visibility=View.GONE
+
+            holder.constLayout.apply {
+                // setear el nuevo foreground del botón
+                foreground = ContextCompat.getDrawable(context, R.drawable.mapa)
+
+
+            }
+            holder.childRecyclerView.setHasFixedSize(true)
+        }else{
+            val parentItem = parentList[position]
+            holder.titleTv.text = parentItem.title
+            holder.childRecyclerView.setHasFixedSize(true)
+            holder.childRecyclerView.layoutManager = GridLayoutManager(holder.itemView.context, 3)
+            val adapter = ChildAdapter(parentItem.mList)
+            holder.childRecyclerView.adapter = adapter
+        }
+
     }
 
     override fun getItemCount(): Int {
